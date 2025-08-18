1ebf538e7b39cea721942dd1beed26e0b7b2259d
Klef29 <albertasare212@gmail.com>
2025-08-18 17:08:59 +0000

    set up the state + the Handler

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,15 +1,29 @@
 import React from "react";
+import { useState } from "react";
+
 
 export const SearchBar = () => {
+  const [query, setQuery] = useState(""); 
+
+  const handleSearch = () => {
+    if (query.trim() === "") {
+      alert("Please enter a search term"); 
+      return;
+    }
+    console.log("Searching for:", query);
+  };
   return (
   <div className="flex flex-col items-center mt-5 space-y-3">
       <input
         className="border border-gray-400 rounded-xl p-3 w-6/12"
         type="text"
         placeholder="Search books..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
       />
       <button
         className="mt-4 bg-[#16247D] text-white font-semibold text-sm w-1/12 py-3 rounded-lg transition duration-300 hover:bg-blue-900"
+        onClick={handleSearch}
       >
         SEARCH
       </button>
